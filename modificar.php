3c7eef5964dd48887891417d14ef5e78c2c2018c
Francisco Andres <andrewplanet@gmail.com>
2017-09-05 08:59:53 -0400

    Update modificar.php

--- modificar.php
+++ modificar.php
@@ -1,30 +1,34 @@
 <?php
 
-echo "<!doctype html>
-					<html lan= 'esp'>
-						<head>
-							<meta charset='utf-8'/>
-							<link rel='stylesheet' type= 'text/css' href='css/animations.css'>
-						</head>
-						<body>
-							  <div class='titulazo'>
-							    <h2></h2>
-							      <div class='search'>
-							        <form action='buscar.php' method='POST'>  
-							          <label class= 'labelazo' for= 'busqueda'>Buscar Empleado:</label>
-							            <input type='search' name='buscar' class='txtlogin'>
-							            <input type='submit' value='buscar' class='envio'>
-							        </form>
-							      </div>
-							  </div>
-							 <div id='menuv'>
-								  <ul>
-								      <li><a href='#menuv' id='search'>Actualizar Empleados</a></li>
-								      <li><a href='#menuv' id='update'>Buscar</a></li>
-								      <li><a href='#menuv' id='delete'>Borrar</a></li>    
-								  </ul>
-							</div>
-							<div>
-								
-";
+	echo "Mejorado";
+
 ?>
+
+<!doctype html>
+<html lang="es">
+	<head>
+		<meta charset='utf-8'/>
+		<link rel='stylesheet' type= 'text/css' href='css/animations.css'>
+		<title>Modificar</title>
+	</head>
+	<body>
+			<div class='titulazo'>
+				<h2></h2>
+					<div class='search'>
+						<form action='buscar.php' method='POST'>  
+							<label class= 'labelazo' for= 'busqueda'>Buscar Empleado:</label>
+								<input type='search' name='buscar' class='txtlogin'>
+								<input type='submit' value='buscar' class='envio'>
+						</form>
+					</div>
+			</div>
+		 <div id='menuv'>
+				<ul>
+						<li><a href='#menuv' id='search'>Actualizar Empleados</a></li>
+						<li><a href='#menuv' id='update'>Buscar</a></li>
+						<li><a href='#menuv' id='delete'>Borrar</a></li>    
+				</ul>
+		</div>
+		<div>
+	</body>
+</html>
